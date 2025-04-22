68e286cd75ca1308630cf506e545e05d26e2f7be
princepatel-patelji <princeroyal4796@gmail.com>
2025-04-22 19:03:27 +0530

    Create aap.py
    Signed-off-by: princepatel-patelji <princeroyal4796@gmail.com>

---
+++ aap.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name="Your Name", partner="Era", year=2025, love_messages=["We met...", "Our first date...", "Forever together..."])
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/letter')
+def letter():
+    return render_template('letter.html')
+
+@app.route('/confession')
+def confession():
+    return render_template('confession.html')
+
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/love')
+def love():
+    return render_template('love.html')
+
+@app.route('/love_card')
+def love_card():
+    return render_template('love_card.html')
+
+@app.route('/romance')
+def romance():
+    return render_template('romance.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
